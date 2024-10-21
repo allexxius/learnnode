bdba9498e2cfa78844abc832f8eb2610a16814b5
Aleks Nazarenko <nazaren.aleks@gmail.com>
2024-10-21 12:52:23 +0300

    es vs commonjs

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const fs = require('fs');
-fs.writeFileSync('test.txt', 'helo node');
+//const fs = require('fs'); // commonjs module
+//import fs from 'fs'; // es module
+//fs.writeFileSync('test.txt', 'hello node');
+
+const common = require('./common.js');
+common.hello();
+import es from './es.js';
+es.hello()
