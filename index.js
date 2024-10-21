a30df2ad5f6fececa430efbfe947a2792e8f64e2
Aleks Nazarenko <nazaren.aleks@gmail.com>
2024-10-21 13:25:07 +0300

    add boxen

--- index.js
+++
@@ -1,9 +0,0 @@
-//const fs = require('fs'); // commonjs module
-//import fs from 'fs'; // es module
-//fs.writeFileSync('test.txt', 'hello node');
-
-
-const common = require('./common.js');
-common.hello();
-const es = require('./es.js').default;
-es.hello()
