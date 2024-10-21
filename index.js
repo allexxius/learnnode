45ffe03a589af7520999ed83df0ce16e3f43bde8
Aleks Nazarenko <nazaren.aleks@gmail.com>
2024-10-21 12:58:27 +0300

    can use require on es module

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 //import fs from 'fs'; // es module
 //fs.writeFileSync('test.txt', 'hello node');
 
-//const common = require('./common.js');
-import common from './common.js';
+
+const common = require('./common.js');
 common.hello();
-import es from './es.js';
+const es = require('./es.js').default;
 es.hello()
