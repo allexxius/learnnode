ee68aed7460ffa9a72b4297ccf853458b91dd449
Aleks Nazarenko <nazaren.aleks@gmail.com>
2024-10-21 12:54:48 +0300

    can use es on commonjs modules

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 //import fs from 'fs'; // es module
 //fs.writeFileSync('test.txt', 'hello node');
 
-const common = require('./common.js');
+//const common = require('./common.js');
+import common from './common.js';
 common.hello();
 import es from './es.js';
 es.hello()
