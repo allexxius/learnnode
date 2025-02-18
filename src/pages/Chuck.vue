7670896cf2240b664ae127c42ba72d3480949052
Aleks Nazarenko <nazaren.aleks@gmail.com>
2025-02-18 11:33:35 +0200

    add axios example

--- src/pages/Chuck.vue
+++ src/pages/Chuck.vue
@@ -1,15 +1,31 @@
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
 
 
 let joke = ref('');
 
-fetch('https://api.chucknorris.io/jokes/random').then(response => {
-    return response.json();
-}).then(data => {
-    console.log(data);
-    joke.value = data.value;
+axios.get('https://api.chucknorris.io/jokes/random').then(res => {
+    joke.value = res.data.value;
+}).catch(err => {
+    console.log(err);
 });
+
+
+//try {
+//let response = await axios.get('https://api.chucknorris.io/jokes/random');
+//joke.value = response.data.value;
+//} catch (err) {
+//    console.log(err);
+//}
+
+
+//fetch('https://api.chucknorris.io/jokes/random').then(response => {
+//    return response.json();
+//}).then(data => {
+//    console.log(data);
+//    joke.value = data.value;
+//});
 
 </script>
 
